8a720e977af1a05f2a1ec1a343bdc403cd8240ac
Dimitri Kalinine <dimitri@pcDim.localdomain>
2026-03-29 15:41:53 +0200

    Rajout logo en filigranne

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="slidev-layout default">
+    <div class="watermark"></div>
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.slidev-layout.default {
+  position: relative;
+}
+
+.watermark {
+  position: absolute;
+  inset: 0;
+  background-image: url('/paf_clean_circle.png');
+  background-repeat: no-repeat;
+  background-position: right 6% center;
+  background-size: 34%;
+  opacity: 0.045;
+  filter: grayscale(100%) contrast(90%);
+  pointer-events: none;
+}
+
+.slidev-layout.default > *:not(.watermark) {
+  position: relative;
+  z-index: 1;
+}
+</style>
